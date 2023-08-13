24535e538703965519951f63e55d11bbbe09e99d
min yeo <korail7d@gmail.com>
2023-08-13 20:17:40 +0900

    test

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          테스트 by editing&nbsp;
+          테스트 by 뭐야&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
